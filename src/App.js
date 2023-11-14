7902f48ed80b2693848b1b5dc66e122ea63cc7ed
Rolande <89341905+Lande21@users.noreply.github.com>
2023-11-13 21:40:00 -0600

    Adding a new application todo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import './App.css';
-import Task from './Task';
+// App.js
 
-function App() {
+import React, { useState } from 'react';
+import TaskList from './TaskList'; // Adjust the path based on your project structure
+
+const App = () => {
+  // State for tasks
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'Wake Up', completed: false },
+    { id: 2, name: 'Say my Prayers', completed: true },
+    { id: 3, name: 'Drink water', completed: true },
+    { id: 4, name: 'Scroll for 20 min', completed: true },
+    { id: 5, name: 'Shower', completed: true },
+    { id: 6, name: 'Put on my deodorant and fragance', completed: true },
+    { id: 7, name: 'Make breakfast', completed: true },
+    { id: 8, name: 'Pack my lunch', completed: true },
+    { id: 9, name: 'Go to work', completed: true },
+    { id: 10, name: 'Back from work, Change clothes', completed: true },
+    { id: 11, name: 'Dinner, do the dishes', completed: true },
+    { id: 12, name: 'Git Commit at least three tasks', completed: true },
+    { id: 13, name: 'Watch TV/ Movie', completed: true },
+    { id: 14, name: 'Say my Prayers', completed: true },
+    { id: 15, name: 'go to bed', completed: true },
+    // ... more tasks
+  ]);
+
+  // Function to handle task toggle
+  const handleToggle = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  // Function to handle task deletion
+  const handleDelete = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
   return (
     <div className="App">
-      <Task />
+      <h1>React To-Do List</h1>
+      <TaskList tasks={tasks} onToggle={handleToggle} onDelete={handleDelete} />
     </div>
   );
-}
+};
 
 export default App;
--- src/Task.js
+++ src/Task.js
@@ -1,17 +1,24 @@
+// Task.js
+
 import React from 'react';
 
-const Task = ({ task, onToggle, onDelete}) => {
-    return (
-        <div className = {`task ${task.completed ? 'completed' : ''} `}>
-            <input
-            type="checkbox"
-            checked={task.completed}
-            onChange={() => onToggle(task.id)}
-            />
-            <span className="task-name">{task.name}</span>
-            <button onClick={() => onDelete(task.id)}>Delete</button>
-        </div>
-    );
+const Task = ({ task, onToggle, onDelete }) => {
+  // Check if task is undefined or doesn't have a completed property
+  if (!task || !task.hasOwnProperty('completed')) {
+    return null; // or some default content, or handle the error in another way
+  }
+
+  return (
+    <div className={`task ${task.completed ? 'completed' : ''}`}>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={() => onToggle(task.id)}
+      />
+      <span className="task-name">{task.name}</span>
+      <button onClick={() => onDelete(task.id)}>Delete</button>
+    </div>
+  );
 };
 
 export default Task;
